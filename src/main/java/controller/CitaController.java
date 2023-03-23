581f12970c0d56e7a3c68c4f4ac3e410015c6374
Juan Carlos Dominguez <juancarlosdominguez@MacBook-Pro-de-Juan.local>
2023-03-22 20:59:56 -0600

    UPDATE - Se regresaron los cambios en getAll

--- src/main/java/controller/CitaController.java
+++ src/main/java/controller/CitaController.java
@@ -15,9 +15,9 @@ public class CitaController {
     @Autowired
     CitaService service;
 
-    @GetMapping("list")
-    public String getList(){
-        return "asdasdasdasd";
+    @GetMapping()
+    public List<Cita> getList(){
+        return service.getAll();
     }
 
     @GetMapping
